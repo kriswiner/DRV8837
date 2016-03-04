42e2846d784f1452ef0d17d85715c604dcd4e14d
Kris Winer <kriswiner@users.noreply.github.com>
2016-03-04 10:03:07 -0800

    Update DRV8837.motorcontrol.ino

--- DRV8837.motorcontrol.ino
+++ DRV8837.motorcontrol.ino
@@ -8,17 +8,18 @@ const int F1 = 3;
 const int F2 = 23;
 const int F3 = 21;
 const int F4 = 5;
-const int R1 = 22;
-const int R2 = 4;
+const int R1 = 4;
+const int R2 = 22;
 const int R3 = 20;
 const int R4 = 6;
-const int POT = 19;
+const int POT = 19; // if you put a potentiometer on a GPIO, you can set the PWMOUT this way as a test
 
 void setup()
 {
+  
   // define motor driver sleep pin as output
   pinMode(Sleep, OUTPUT);
-  // Enable DRV8837motor driver
+  // Enable DRV8837 motor driver
   digitalWrite(Sleep, HIGH);
   
   // define motor control pins as output
@@ -31,19 +32,38 @@ void setup()
   pinMode(R3, OUTPUT);
   pinMode(R4, OUTPUT);
   pinMode(POT, INPUT);
- 
+
+ analogWrite(R1, 0);
+ analogWrite(R2, 0);
+ analogWrite(R3, 0);
+ analogWrite(R4, 0);
  
 }
 
 void loop()
 {
- PWMout = analogRead(POT)/4;
+// PWMout = analogRead(POT)/4;
  
-// Serial.println(PWMout);
+ for(int ii = 1; ii < 256; ii++) {  // slowly ramp the motors to full speed then ramp down
+   PWMout = ii;
+   analogWrite(F1, PWMout);
+   analogWrite(F2, PWMout);
+   analogWrite(F3, PWMout);
+   analogWrite(F4, PWMout);
+   delay(200);
+ }
  
-  // run PWM to all four forward motor control pins for test
- analogWrite(F1, PWMout);
- analogWrite(F2, PWMout);
- analogWrite(F3, PWMout);
- analogWrite(F4, PWMout);
+ delay(2000);
+ 
+  for(int ii = 1; ii < 256; ii++) {
+   PWMout =  256 - ii;
+   analogWrite(F1, PWMout);
+   analogWrite(F2, PWMout);
+   analogWrite(F3, PWMout);
+   analogWrite(F4, PWMout);
+   delay(200);
+ }
+ 
+  delay(2000);
+ 
 }
